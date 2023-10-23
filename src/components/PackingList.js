1535566328890ee1a4bcfde83b72ef6aa690facf
Nivedita <nivedita.pagar@outlook.com>
2023-10-23 13:47:09 +0200

    Add Static Sorting Dropdown and Clear List Button (UI Only)

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -13,6 +13,15 @@ export default function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+
+        <button>Clear List</button>
+      </div>
     </div>
   );
 }
