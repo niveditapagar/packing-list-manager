1f476e5a6f526797fb9594bf4f3dab5146657791
Nivedita <nivedita.pagar@outlook.com>
2023-10-23 13:09:05 +0200

    Pass Items to Stats Component, Implement Dynamic Stats Handling

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,23 @@
-export default function Stats() {
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPackedItems = items.filter((i) => i.packed).length;
+  const percentPacked = Math.round((numPackedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      You have x items in your list and you've already packed x (x%)
+      <em>
+        {percentPacked === 100
+          ? "You've got everything! You're ready to go ✈️"
+          : `💼 You have ${numItems} items in your list, and you have already packed
+            ${numPackedItems} (${percentPacked ? percentPacked : 0}%)`}
+      </em>
     </footer>
   );
 }
