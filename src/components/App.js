e962c272964137f55792c63b453a34cb14f3f39a
Nivedita <nivedita.pagar@outlook.com>
2023-10-23 12:46:56 +0200

    Add State for Items, Implement handleAddItem in App.js, and Use in Form.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
+      <Form onAddItems={handleAddItems} />
       <PackingList />
       <Stats />
     </div>
