a86f522ee628018ea765470026d7a96f7f66ed08
Nivedita <nivedita.pagar@outlook.com>
2023-10-23 12:37:01 +0200

    Implement partial Form Component with Quantity and Description Handling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    // TODO handle submission
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip 😍 ?</h3>
-      <select>
-        <option value={1} key={1}>
-          {1}
-        </option>
-        <option value={2} key={2}>
-          {2}
-        </option>
-        <option value={3} key={3}>
-          {3}
-        </option>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
