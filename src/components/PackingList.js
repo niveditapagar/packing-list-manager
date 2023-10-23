f4deb64a3ba7692475b400b77fd72c6f11e1c586
Nivedita <nivedita.pagar@outlook.com>
2023-10-23 18:09:10 +0200

    Implement Item Sorting with Input Time as Default

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Item from "./Item";
 
 export default function PackingList({
@@ -6,10 +7,29 @@ export default function PackingList({
   onToggleItem,
   onClearList,
 }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -19,7 +39,7 @@ export default function PackingList({
         ))}
       </ul>
       <div className="actions">
-        <select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
